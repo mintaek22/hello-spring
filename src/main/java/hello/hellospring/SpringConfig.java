dae8e6f33da68dd09e7419a9e5c374d5cb0aecc1
mintaek22 <biba99@naver.com>
2022-07-12 22:05:15 +0900

    day5

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,11 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 
 //자바 코드로 스프링 빈 등록
@@ -23,12 +21,16 @@ public class SpringConfig {
     }
 
 
-
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
 
+    //Aop는 다른 것 들과 다르게 정형화된 툴을 아니여서 따로 bean을 등록 하는게 좋다(!=Component Scan)
+    @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }
   /*  @Bean
     public MemberRepository memberRepository(){
        // return new MemoryMemberRepository();
---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class TimeTraceAop {
+
+    //해당 pattern 의 method 실행 전후 모두에서 동작
+    @Around("execution(* hello.hellospring..*(..)) && !target(hello.hellospring.SpringConfig)")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+        System.out.println("START: "+joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: "+joinPoint.toString()+" "+timeMs+"ms");
+        }
+
+    }
+}
